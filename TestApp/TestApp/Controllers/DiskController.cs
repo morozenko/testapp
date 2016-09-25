b7d917c9076257f045c5493f85f512a33d4d13b7
Andrii <Andrii>
2016-09-25 12:46:40 +0300

    Added app project

---
+++ TestApp/TestApp/Controllers/DiskController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.Results;
+
+namespace TestApp.Controllers
+{
+    public class DiskController : ApiController
+    {
+        private string[] drives;
+
+        public DiskController ()
+        {
+            drives = System.Environment.GetLogicalDrives();
+        }
+
+        [ResponseType(typeof(string[]))]
+        public async Task<IHttpActionResult> Get()
+        {
+            return this.Ok(drives);
+        }
+
+        [ResponseType(typeof(Dictionary<string, string[]>))]
+        public async Task<IHttpActionResult> Get(string directory)
+        {
+            string[] dirs = GetDirectoryDirs(directory);
+            string[] files = GetDirectoryFiles(directory);
+
+            int[] sizes = GetSizes(directory);
+            string[] size = {"0", "0", "0"};
+
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                size[i] = sizes[i].ToString();
+            }
+            
+
+            var response = new Dictionary<string, string[]>();
+            response["folders"] = dirs;
+            response["files"] = files;
+            response["sizes"] = size;
+
+            return this.Ok(response);
+        }
+
+        protected string[] GetDirectoryDirs(string rootDirectory)
+        {
+            string[] subDirs = null;
+            try
+            {
+                subDirs = System.IO.Directory.GetDirectories(rootDirectory);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                ;
+            }
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                ;
+            }
+            catch (SystemException)
+            {
+                ;
+            }
+
+            return subDirs;
+        }
+
+        protected string[] GetDirectoryFiles(string rootDirectory)
+        {
+            string[] files = null;
+
+            try
+            {
+                files = System.IO.Directory.GetFiles(rootDirectory);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                ;
+            }
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                ;
+            }
+            catch (SystemException)
+            {
+                ;
+            }
+
+            return files;
+        }
+
+        // return array of int[3] where
+        // int[0] amount of items <=10Mb
+        // int[1] amount of items > 10mb AND <= 50mb
+        // int[2] amount of items >= 100mb
+        protected int[] GetSizes(string rootDir)
+        {
+            int MEGABYTE = 1024 * 1024;
+            int[] sizes = { 0, 0, 0 };
+
+            string[] subDirs = null;
+            /*try
+            {
+                subDirs = GetDirectoryDirs(rootDir);
+
+                if (null != subDirs)
+                {
+                    foreach (string dir in subDirs)
+                    {
+                        int[] subsize = GetSizes(dir);
+                        sizes[0] += subsize[0];
+                        sizes[1] += subsize[1];
+                        sizes[2] += subsize[2];
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                ;
+            }
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                ;
+            }
+            catch (SystemException)
+            {
+                ;
+            }*/
+
+            string[] files = GetDirectoryFiles(rootDir);
+
+            if (null != files)
+            {
+                foreach (string path in files)
+                {
+                    try
+                    {
+                        long length = new System.IO.FileInfo(path).Length;
+
+                        if (10 * MEGABYTE >= length)
+                        {
+                            sizes[0]++;
+                        }
+                        else if ((10 * MEGABYTE < length) && (length < 50 * MEGABYTE))
+                        {
+                            sizes[1]++;
+                        }
+                        else if (100 * MEGABYTE <= length)
+                        {
+                            sizes[2]++;
+                        }
+                    }
+                    catch (UnauthorizedAccessException e)
+                    {
+                        ;
+                    }
+                    catch (System.IO.DirectoryNotFoundException e)
+                    {
+                        ;
+                    }
+                    catch (SystemException)
+                    {
+                        ;
+                    }
+                }
+            }
+
+            return sizes;
+        }
+    }
+}
---
+++ TestApp/TestApp/Scripts/disk_controller.js
@@ -0,0 +1,82 @@
+﻿angular.module("TestApp", [])
+    .controller("diskController", function ($scope, $http) {
+        $http.get("/api/disk").then(function (response) {
+            $scope.items = response.data;
+            $scope.curPath = "";
+            $scope.sizes = [0,0,0];
+        },
+        function (response) {
+            alert("Can't read disk info");
+        });
+
+        $scope.getDirectory = function (dir) {
+            if (".." === dir) { // go to one level up
+                var arr = $scope.curPath.split('\\');
+                arr.pop();
+                $scope.curPath = arr.join('\\');
+            }
+            else {
+                if (0 == $scope.curPath.length) {   // don't prepend extra slash to disk names
+                    $scope.curPath = dir;
+                }
+                else {
+                    $scope.curPath = $scope.curPath + "\\" + dir;
+                }
+            }
+
+            // check whether we can't go up in folder tree
+            if ($scope.curPath.includes('\\')) {
+                $http.get("/api/disk/?directory=" + $scope.curPath).then(function (response) {
+                    $scope.items = [];  // clear folders
+                    $scope.folderfiles = []; // clear files
+                    $scope.items[0] = "..";
+                    $scope.sizes = response.data.sizes;
+                    console.log(response.data.sizes);
+
+                    // iterate over folders
+                    for (i = 0; i < response.data.folders.length; i++) {
+                        var item = response.data.folders[i].split('\\').pop();
+                        $scope.items[i + 1] = item;
+                    };
+
+                    // iterate over files
+                    for (i = 0; i < response.data.files.length; i++) {
+                        var file = response.data.files[i].split('\\').pop();
+                        console.log(file);
+                        $scope.folderfiles[i] = file;
+                    };
+                },
+                function (response) {
+                    alert("Folder doesn't exist or some other error happened");
+
+                    // cleanup path
+                    var arr = $scope.curPath.split('\\');
+                    arr.pop();
+                    $scope.curPath = arr.join('\\');
+
+                    // check for top disk case
+                    if (!$scope.curPath.includes('\\'))
+                    {
+                        $scope.curPath = "";
+                    }
+
+                    console.log($scope.curPath);
+                });
+            }
+            else // need to show logical disks
+            {
+                $scope.items = [];  // clear folders
+                $scope.folderfiles = []; // clear files
+                $http.get("/api/disk/").then(function (response) {
+                    $scope.items = response.data;
+                    $scope.curPath = "";
+                    $scope.sizes = [0, 0, 0];
+                },
+                function (response) {
+                    alert("Can't read disk info");
+
+                    $scope.curPath = "";
+                });
+            }
+        }; // getDirectory
+    });
